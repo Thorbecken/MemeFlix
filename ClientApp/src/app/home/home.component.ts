25a9469b4ea7c582333852c2d1314a850e3897dd
Thorbecken <31536198+Thorbecken@users.noreply.github.com>
2018-10-15 13:07:23 +0200

    Initial commit

---
+++ ClientApp/src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+}
+
+export class FetchDataComponent {
+  public memes: Memes[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Memes[]>(baseUrl + 'api/EmbeddedVideo/Memes').subscribe(result => {
+      this.memes = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Memes {
+  url: String;
+}
---
+++ Controllers/EmbeddedVideoController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MemeFlix.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmbeddedVideoController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "https://www.youtube.com/embed/kfVsfOSbJY0", "https://www.youtube.com/embed/dQw4w9WgXcQ" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<Meme> Memes()
+        {
+            return Enumerable.Range(1, memeStrings.Length).Select(x => new Meme(memeStrings[x]));
+        }
+        /*public string[] Memes()
+        {
+            return memeStrings;
+        }*/
+
+        public static string[] memeStrings = new[]
+        {
+            "https://www.youtube.com/embed/kfVsfOSbJY0", "https://www.youtube.com/embed/dQw4w9WgXcQ"
+        };
+
+        public class Meme
+        {
+            public string url { get; set; }
+
+            public Meme(string url)
+            {
+                this.url = url;
+            }
+        }
+    }
+}
