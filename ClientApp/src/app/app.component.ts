f1cf22258868c6d88a1f00bbef6f4c047cad487d
Thorbecken <31536198+Thorbecken@users.noreply.github.com>
2018-10-17 20:23:41 +0200

    error galore

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,22 +1,36 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-//import BASE_URL from './app.module';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'MemeFlix';
 
   public memes: Memes[];
-  constructor(private http: HttpClient) { //constructor(private http: HttpClient, @Inject(BASE_URL) baseUrl: string) { 
-    http.get<Memes[]>('https://localhost:44333/api/EmbeddedVideo/Memes').subscribe(result => { //    http.get<Memes[]>(baseUrl + 'api/EmbeddedVideo/Memes').subscribe(result => { 
-      this.memes = result;
-    }, error => console.error(error));
+  public videourl: SafeResourceUrl[];
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    let id = 'an-id-goes-here';
+    let url = `https://www.youtube.com/embed/${id}`;
+
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
-}
+
+  ngOnDestroy() { }
+  public memes: Memes[];
+  constructor(private http: HttpClient) { //constructor(private http: HttpClient, @Inject(BASE_URL) baseUrl: string) { 
+  http.get<Memes[]>('https://localhost:44333/api/EmbeddedVideo/Memes').subscribe(result => { //    http.get<Memes[]>(baseUrl + 'api/EmbeddedVideo/Memes').subscribe(result => { 
+  this.memes = result;
+   }, error => console.error(error));
+  }
+
 
 interface Memes {
   url: String
