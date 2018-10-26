b70ba5761fee95625f8708028d3c71c653402be1
Thorbecken <31536198+Thorbecken@users.noreply.github.com>
2018-10-26 14:16:27 +0200

    first draft

--- ClientApp/src/app/Module/admin/admin.module.ts
+++ ClientApp/src/app/Module/admin/admin.module.ts
@@ -3,6 +3,7 @@ import { AdminRoutingModule } from "./admin-routing.module";
 import { AdminComponent } from "./admin.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { NgModule } from '@angular/core';
+import { EditorComponent } from './components/editor/editor.component';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { NgModule } from '@angular/core';
   ],
   declarations: [
     AdminComponent,
-    DashboardComponent
+    DashboardComponent,
+    EditorComponent
   ]
 })
 export class AdminModule { }
